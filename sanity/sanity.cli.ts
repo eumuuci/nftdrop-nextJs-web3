47aec86f4338581e4d30815f9adf01720c0b13f1
Nicolas Sousa <nicksantossousa22@gmail.com>
2023-01-31 19:11:17 -0300

    databaseDeployed

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '1d6a4fnw',
+    dataset: 'production'
+  }
+})
--- src/pages/nft/[id].tsx
+++ src/pages/nft/[id].tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useMetamask, useAddress , useDisconnect } from "@thirdweb-dev/react";
+import { GetServerSideProps } from 'next';
+import { sanityClient, urlFor } from 'sanity';
+import { Collection } from 'typings';
+import Link from 'next/link';
 
-const NftDropPage = () => {
+interface Props {
+  collection: Collection
+}
+
+const NftDropPage = ({collection}: Props) => {
   const ConnectWithMetamask = useMetamask();
   const Address = useAddress();
   const Disconnect = useDisconnect();
@@ -14,11 +22,11 @@ const NftDropPage = () => {
           <div className='bg-[#1b263b] p-2 rounded-xl'>
             <img
               className='w-44 rounded-xl object-cover lg:h-96 lg:w-74'
-              src='https://i.pinimg.com/564x/53/03/a7/5303a77535aec49639383b0d1bf74ff1.jpg' />
+              src={urlFor(collection.previewImage).url()} />
           </div>
           <div className='text-center p-5 space-y-2'>
-            <h1 className='text-3xl font-bold text-white'>STAR react</h1>
-            <h2 className='text-xl text-gray-300'>A collection of STAR react</h2>
+            <h1 className='text-3xl font-bold text-white'>{collection?.nftCollectionName}</h1>
+            <h2 className='text-xl text-gray-300'>{collection?.description}</h2>
           </div>
         </div>
       </div>
@@ -27,12 +35,14 @@ const NftDropPage = () => {
       <div className='flex flex-1 flex-col p-12 lg:col-span-6'>
       {/* header */}
       <header className='flex items-center justify-between'>
+        <Link href={'/'}>
         <h1 className='w-52 cursor-pointer text-xl font-extralight
         sm:w-80'>
           The{' '}
           <span className='font-extrabold underline decoration-[#415a77]'>STAREACT</span>
           {' '}NFT Market Place
           </h1>
+        </Link>
         <button 
         onClick={() => (Address ? Disconnect() : ConnectWithMetamask())}
         className='rounded-full bg-[#1b263b] px-4 text-white py-2 text-xs
@@ -50,19 +60,62 @@ const NftDropPage = () => {
       lg:space-y-0 lg:justify-center'>
         <img 
         className='w-80 object-cover pb-10 lg:h-40'
-        src='https://nftevening.com/wp-content/uploads/2022/05/punkscape-6144.png.webp'/>
+        src={urlFor(collection.mainImage).url()}/>
         <h1 className='text-3xl font-bold lg:text-5xl lg:font-extrabold'>
-          The STAR pixel collection | NFT DROP
+          {collection?.title}
         </h1>
 
         <p className='pt-2 text-xl text-green-500'>14 / 20 NFT's claimed</p>
       </div>
       {/* mint button */}
       <button className='h-16 w-full bg-[#778da9] text-white rounded-full mt-10 font-bold'
-      >Mint NFT (0.001 ETH)</button>
+      >Mint NFT (0.01 ETH)</button>
       </div>
     </div>
   )
 }
 
 export default NftDropPage;
+
+export const getServerSideProps: GetServerSideProps = async ({params}) => {
+  const query = `*[_type == "collection" && slug.current == $id][0]{
+    _id,
+    title,
+    address,
+    description,
+    nftCollectionName,
+    mainImage {
+      asset
+    },
+    previewImage {
+      asset
+    },
+    slug {
+      current
+    },
+    creator-> {
+      _id,
+      name,
+      address,
+      slug {
+        current
+      },
+    },
+  }`
+
+  const collection = await sanityClient.fetch(query, {
+    id: params?.id
+  })
+
+  if(!collection) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      collection
+    }
+  }
+}
